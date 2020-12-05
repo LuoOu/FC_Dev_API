b5f30ca9979ecc65255e4fa296f983efbb717705
LuoOu <49121878+LuoOu@users.noreply.github.com>
2020-12-05 17:57:36 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -4,12 +4,6 @@
 # for Sphinx-1.4 or newer
 extensions = ['recommonmark']
 
-# for Sphinx-1.3
-from recommonmark.parser import CommonMarkParser
-
-source_parsers = {
-    '.md': CommonMarkParser,
-}
 
 source_suffix = ['.rst', '.md']
 
